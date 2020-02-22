b57c8528c8b218340a0778cfcebd8f76a7ba6422
Luu Gia Thuy <luugiathuy@gmail.com>
2020-02-22 21:35:24 +0800

    Chapter 4 Ray struct, a Simple Camera and Background
    'https://raytracing.github.io/books/RayTracingInOneWeekend.html#rays,asimplecamera,andbackground'

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+mod ray;
 mod vec3;
 
+use ray::Ray;
 use vec3::Vec3;
+
+fn color(r: &Ray) -> Vec3 {
+    let unit_direction = r.direction.to_unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Vec3(1.0, 1.0, 1.0) + t * Vec3(0.5, 0.7, 1.0)
+}
 
 fn main() {
     let nx = 200;
     let ny = 100;
     println!("P3\n{} {}\n255", nx, ny);
+
+    let lower_left_corner = Vec3(-2.0, -1.0, -1.0);
+    let horizontal = Vec3(4.0, 0.0, 0.0);
+    let vertical = Vec3(0.0, 2.0, 0.0);
+    let origin = Vec3(0.0, 0.0, 0.0);
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let col = Vec3(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let r = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical);
+            let col = color(&r);
             let ir = (255.99 * col.r()) as i32;
             let ig = (255.99 * col.g()) as i32;
             let ib = (255.99 * col.b()) as i32;
---
+++ src/ray.rs
@@ -0,0 +1,36 @@
+use super::vec3::Vec3;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Ray {
+  pub origin: Vec3,
+  pub direction: Vec3,
+}
+
+impl Ray {
+  pub fn new(o: Vec3, d: Vec3) -> Ray {
+    Ray {
+      origin: o,
+      direction: d,
+    }
+  }
+
+  pub fn point_at_parameter(&self, t: f32) -> Vec3 {
+    self.origin + t * self.direction
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn point_at_parameter() {
+    let o = Vec3(1.0, 2.0, 3.0);
+    let d = Vec3(1.0, 1.0, 0.0);
+    let r = Ray::new(o, d);
+    let p = r.point_at_parameter(5.0);
+    assert_eq!(6.0, p.x());
+    assert_eq!(7.0, p.y());
+    assert_eq!(3.0, p.z());
+  }
+}
