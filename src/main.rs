a76a56948210276f0fd15176f0d02aa72e09919b
Luu Gia Thuy <luugiathuy@gmail.com>
2020-02-23 15:27:15 +0800

    Chapter 13 Random scene
    'https://raytracing.github.io/books/RayTracingInOneWeekend.html#wherenext?'

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,80 @@ use ray::Ray;
 use sphere::Sphere;
 use vec3::Vec3;
 
-fn color(r: &Ray, world: &Vec<Box<dyn Hittable>>, depth: i32) -> Vec3 {
+fn random_scene() -> Box<dyn Hittable> {
+    let mut rng = rand::thread_rng();
+    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
+    world.push(Box::new(Sphere::new(
+        Vec3(0.0, -1000.0, 0.0),
+        1000.0,
+        Box::new(Lambertian::new(Vec3(0.5, 0.5, 0.5))),
+    )));
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_material = rng.gen::<f32>();
+            let center = Vec3(
+                a as f32 + 0.9 * rng.gen::<f32>(),
+                0.2,
+                b as f32 + 0.9 * rng.gen::<f32>(),
+            );
+            if (center - Vec3(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_material < 0.8 {
+                    // diffuse
+                    world.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Box::new(Lambertian::new(Vec3(
+                            rng.gen::<f32>() * rng.gen::<f32>(),
+                            rng.gen::<f32>() * rng.gen::<f32>(),
+                            rng.gen::<f32>() * rng.gen::<f32>(),
+                        ))),
+                    )));
+                } else if choose_material < 0.95 {
+                    // metal
+                    world.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Box::new(Metal::new(
+                            Vec3(
+                                0.5 * (1.0 + rng.gen::<f32>()),
+                                0.5 * (1.0 + rng.gen::<f32>()),
+                                0.5 * (1.0 + rng.gen::<f32>()),
+                            ),
+                            0.5 * rng.gen::<f32>(),
+                        )),
+                    )));
+                } else {
+                    // glass
+                    world.push(Box::new(Sphere::new(
+                        center,
+                        0.2,
+                        Box::new(Dielectric::new(1.5)),
+                    )));
+                }
+            }
+        }
+    }
+
+    world.push(Box::new(Sphere::new(
+        Vec3(0.0, 1.0, 0.0),
+        1.0,
+        Box::new(Dielectric::new(1.5)),
+    )));
+    world.push(Box::new(Sphere::new(
+        Vec3(-4.0, 1.0, 0.0),
+        1.0,
+        Box::new(Lambertian::new(Vec3(0.4, 0.2, 0.1))),
+    )));
+    world.push(Box::new(Sphere::new(
+        Vec3(4.0, 1.0, 0.0),
+        1.0,
+        Box::new(Metal::new(Vec3(0.7, 0.6, 0.5), 0.0)),
+    )));
+
+    Box::new(world)
+}
+
+fn color(r: &Ray, world: &Box<dyn Hittable>, depth: i32) -> Vec3 {
     match world.hit(r, 0.001, std::f32::MAX) {
         Some(rec) => {
             if depth < 50 {
@@ -35,15 +108,15 @@ fn color(r: &Ray, world: &Vec<Box<dyn Hittable>>, depth: i32) -> Vec3 {
 }
 
 fn main() {
-    let nx = 200;
-    let ny = 100;
+    let nx = 1200;
+    let ny = 800;
     let ns = 100;
     println!("P3\n{} {}\n255", nx, ny);
 
-    let look_from = Vec3(3.0, 3.0, 2.0);
-    let look_at = Vec3(0.0, 0.0, -1.0);
-    let dist_to_focus = (look_from - look_at).length();
-    let aperture = 2.0;
+    let look_from = Vec3(13.0, 2.0, 3.0);
+    let look_at = Vec3(0.0, 0.0, 0.0);
+    let focus_dist = 10.0;
+    let aperture = 0.1;
     let cam = Camera::new(
         look_from,
         look_at,
@@ -51,36 +124,10 @@ fn main() {
         20.0,
         nx as f32 / ny as f32,
         aperture,
-        dist_to_focus,
+        focus_dist,
     );
     let mut rng = rand::thread_rng();
-
-    let mut world: Vec<Box<dyn Hittable>> = Vec::new();
-    world.push(Box::new(Sphere::new(
-        Vec3(0.0, 0.0, -1.0),
-        0.5,
-        Box::new(Lambertian::new(Vec3(0.1, 0.2, 0.5))),
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3(0.0, -100.5, -1.0),
-        100.0,
-        Box::new(Lambertian::new(Vec3(0.8, 0.8, 0.0))),
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3(1.0, 0.0, -1.0),
-        0.5,
-        Box::new(Metal::new(Vec3(0.8, 0.6, 0.2), 0.3)),
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3(-1.0, 0.0, -1.0),
-        0.5,
-        Box::new(Dielectric::new(1.5)),
-    )));
-    world.push(Box::new(Sphere::new(
-        Vec3(-1.0, 0.0, -1.0),
-        -0.45,
-        Box::new(Dielectric::new(1.5)),
-    )));
+    let world = random_scene();
     for j in (0..ny).rev() {
         for i in 0..nx {
             let mut col = Vec3(0.0, 0.0, 0.0);
