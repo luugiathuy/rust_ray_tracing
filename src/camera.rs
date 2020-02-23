eed45a165875eaef3959d2b49af371a55d3066cb
Luu Gia Thuy <luugiathuy@gmail.com>
2020-02-23 13:33:13 +0800

    Chapter 11a Add vertical fov for camera
    'https://raytracing.github.io/books/RayTracingInOneWeekend.html#positionablecamera'

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,6 @@
 use super::ray::Ray;
 use super::vec3::Vec3;
+use std::f32;
 
 #[derive(Clone, Copy, Debug)]
 pub struct Camera {
@@ -10,12 +11,15 @@ pub struct Camera {
 }
 
 impl Camera {
-  pub fn new() -> Self {
+  pub fn new(vertical_fov: f32, aspect: f32) -> Self {
+    let theta = vertical_fov * f32::consts::PI / 180.0;
+    let half_height = f32::tan(theta * 0.5);
+    let half_width = aspect * half_height;
     Camera {
       origin: Vec3(0.0, 0.0, 0.0),
-      lower_left_corner: Vec3(-2.0, -1.0, -1.0),
-      horizontal: Vec3(4.0, 0.0, 0.0),
-      vertical: Vec3(0.0, 2.0, 0.0),
+      lower_left_corner: Vec3(-half_width, -half_height, -1.0),
+      horizontal: Vec3(2.0 * half_width, 0.0, 0.0),
+      vertical: Vec3(0.0, 2.0 * half_height, 0.0),
     }
   }
 
